cd49f6b3402169993c2fc5724f0103716b889176
Eduardo Ferro <eferro@alea-soluciones.com>
2014-06-15 11:58:39 +0200

    Red. Bug found?

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -28,6 +28,18 @@ class GildedRoseTest(unittest.TestCase):
        	self._assert_quality_and_sell_in(items[1], sell_in=19, quality=31)
 
 
+    def test_increase_quality_by_two_for_products_that_get_better_as_they_age_and_there_are_10_days_or_less_left(self):
+    	items = [
+        	Item("Aged Brie", 10, 34),
+        	Item("Backstage passes to a TAFKAL80ETC concert", 8, 30) 
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+       	self._assert_quality_and_sell_in(items[0], sell_in=9, quality=36)
+       	self._assert_quality_and_sell_in(items[1], sell_in=7, quality=32)
+
+
     def _assert_quality_and_sell_in(self, item, sell_in, quality):
     	self.assertEquals(sell_in, item.sell_in)
         self.assertEquals(quality, item.quality)
