2ce567eb92a35b502ae909192a4c723ace060e20
Eduardo Ferro <eferro@alea-soluciones.com>
2014-06-15 11:42:41 +0200

    Tests for products that get better as they age

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -15,6 +15,19 @@ class GildedRoseTest(unittest.TestCase):
        	self._assert_quality_and_sell_in(items[0], sell_in=9, quality=19)
        	self._assert_quality_and_sell_in(items[1], sell_in=2, quality=5)
 
+
+    def test_increases_quality_by_one_for_products_that_get_better_as_they_age(self):
+    	items = [
+        	Item("Aged Brie", 20, 30),
+        	Item("Backstage passes to a TAFKAL80ETC concert", 20, 30) 
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+       	self._assert_quality_and_sell_in(items[0], sell_in=19, quality=31)
+       	self._assert_quality_and_sell_in(items[1], sell_in=19, quality=31)
+
+
     def _assert_quality_and_sell_in(self, item, sell_in, quality):
     	self.assertEquals(sell_in, item.sell_in)
         self.assertEquals(quality, item.quality)
